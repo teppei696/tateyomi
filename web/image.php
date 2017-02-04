0f788db3be30f972e5358115feccbf4b00891325
teppei696 <teppei6969@gmail.com>
2017-02-04 18:59:09 +0900

    文字列もパラメータ

--- web/image.php
+++ web/image.php
@@ -4,11 +4,12 @@ $size = $_GET['size'];
 $x = $_GET['x'];
 $y = $_GET['y'];
 $image = $_GET['image'];
+$text = $_GET['text'];
 
 $img = imagecreatefromjpeg($image);
 
 # 必要に応じてUTF8へ変換(環境依存)
-$text = mb_convert_encoding('文字列を書き込む', 'UTF-8', 'auto');
+$text = mb_convert_encoding($text, 'UTF-8', 'auto');
 
 # 黒い文字を書き込む
 $black = imagecolorallocate($img, 0, 0, 0);
