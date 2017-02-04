55e327dc57c34a71973fc3bf0fc20877ab024520
teppei696 <teppei6969@gmail.com>
2017-02-04 18:45:27 +0900

    パラメータで各種値を設定

--- web/image.php
+++ web/image.php
@@ -1,13 +1,20 @@
 <?php
 
+$size = $_GET['size'];
+$x = $_GET['x'];
+$y = $_GET['y'];
+$fontSize = $_GET['font'];
+$fontSize = $_GET['font'];
+$fontSize = $_GET['font'];
+
 $img = imagecreatefromjpeg('tenki1.jpg');
 
 # 必要に応じてUTF8へ変換(環境依存)
 $text = mb_convert_encoding('文字列を書き込む', 'UTF-8', 'auto');
 
-# 白い文字を書き込む
-$white = imagecolorallocate($img, 0xff, 0xff, 0xff);
-imagettftext($img, 16, 0, 5, 200, $white,'ipagp.ttf', $text);
+# 黒い文字を書き込む
+$black = imagecolorallocate($img, 0, 0, 0);
+imagettftext($img, $size, 0, $x, $y, $black,'ipagp.ttf', $text);
 
 header('Content-Type: image/jpeg');
 imagejpeg($img);
