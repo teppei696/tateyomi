820c2263f548ec07860947aa8e1657b1dffa0302
teppei696 <teppei6969@gmail.com>
2017-02-05 13:23:41 +0900

    文字列だぜー

--- web/image.php
+++ web/image.php
@@ -24,17 +24,16 @@ $json = file_get_contents($url);
 $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 $arr = json_decode($json,true);
 
+$title = "";
 if ($arr === NULL) {
 	return;
 } else {
 	$json_count = count($arr["articles"]);
-	$titles = array();
 	for ($i = 0; $i < $json_count; $i++) {
-		$titles[] = $arr["articles"][$i]["title"];
-		error_log($titles[$i]);
+		$title .= $arr["articles"][$i]["title"];
+		error_log($title);
 	}
 }
-
 
 $text1 = "交際費の５０％非課税効果";
 $text2 = "ある？ＨＫＴ若田部遥卒業";
